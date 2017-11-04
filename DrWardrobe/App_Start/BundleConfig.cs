a71008dd98e0a3c2d474f342c8ed6150ecabab9f
karenmcewen <karenf_email@yahoo.com>
2017-11-04 19:34:24 -0400

    added three data to each of 10 tables. Home page buttons work

--- DrWardrobe/App_Start/BundleConfig.cs
+++ DrWardrobe/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace DrWardrobe
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      
                       "~/Content/site.css"));
         }
     }
